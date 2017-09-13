de9bc8e7cdfa59edb1cae681cb1878d49f09c507
william <wannabemyself@126.com>
2017-09-13 16:36:05 +0800

    modified by wq 2017091316291800

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 
 	public static void   main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("modified by wq 201709131629");
+		System.out.println("modified by wq 201709131800");
 	}
 
 }
